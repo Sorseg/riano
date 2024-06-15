8702a734a7fc43dbf8ce4989a423c6454f62d857
Sorseg <sorsegg@gmail.com>
2024-06-15 15:13:25 +0300

    Add asdr

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{
-    collections::VecDeque, f32::consts::{PI, TAU}, sync::{Arc, Mutex}
+    collections::VecDeque,
+    f32::consts::{PI, TAU},
+    sync::{Arc, Mutex},
 };
 
 use cpal::{
@@ -17,14 +19,50 @@ struct Note {
     phase: f32,
     freq: f32,
     vel: f32,
+    asdr: Asdr,
+    on: bool,
 }
 
-const VIS_BUF_SIZE: usize = 44100 * 1;
+#[derive(Debug)]
+struct Asdr {
+    attack_level: f32,
+    release_level: f32,
+    attack: f32,
+    release: f32,
+}
+
+impl Asdr {
+    fn attack(&mut self, t: f32) -> f32 {
+        if self.attack_level < 1.0 {
+            self.attack_level += t / self.attack;
+        }
+
+        self.attack_level.clamp(0.0, 1.0)
+    }
+
+    fn release(&mut self, t: f32) -> f32 {
+        // TODO: separate mutation and reading
+        self.release_level *= 0.1_f32.powf(t / self.release);
+        self.release_level
+    }
+
+    fn new(attack: f32, release: f32) -> Self {
+        Self {
+            attack,
+            release,
+            attack_level: 0.0,
+            release_level: 1.0,
+        }
+    }
+}
+
+const VIS_BUF_SIZE: usize = 44100 * 5;
 
 fn main() {
     // FIXME: opt
     let notes = Arc::new(Mutex::new(Vec::<Note>::new()));
     let notes2 = Arc::clone(&notes);
+    let notes3 = Arc::clone(&notes);
 
     let buf = Arc::new(Mutex::new(VecDeque::<f32>::new()));
     let buf2 = Arc::clone(&buf);
@@ -38,6 +76,7 @@ fn main() {
         .expect("casio not connected");
 
     println!("Connecting to {}", midi_in.port_name(casio).unwrap());
+    let mut note_counter: u64 = 1;
     let _conn = midi_in
         .connect(
             casio,
@@ -49,13 +88,18 @@ fn main() {
                     ..
                 } = m
                 {
+                    // PRNG
+                    let detune = (note as f32 * 1.523644) % 1.0;
                     let note = Note {
                         note_n: note,
-                        freq: (note - 21) as f32 * 10.0,
+                        on: true,
+                        freq: 440.0 * 2_f32.powf((note as f32 + detune * 0.1 - 69.0) / 12.0),
                         vel: velocity as f32 / 255.0,
                         phase: 0.0,
+                        asdr: Asdr::new(0.01, 1.0),
                     };
-                    println!("note {note:?}");
+                    note_counter += 1;
+                    println!("{note_counter} {note:?}");
                     notes.lock().unwrap().push(note)
                 }
                 if let MidiMsg::ChannelVoice {
@@ -63,7 +107,12 @@ fn main() {
                     ..
                 } = m
                 {
-                    notes.lock().unwrap().retain(|n| n.note_n != note)
+                    notes.lock().unwrap().iter_mut().for_each(|n| {
+                        if n.note_n == note && n.on {
+                            n.on = false;
+                            n.asdr.release_level = n.asdr.attack_level;
+                        }
+                    })
                 }
             },
             (),
@@ -75,20 +124,29 @@ fn main() {
     let dev = host.default_output_device().unwrap();
     let mut config: cpal::StreamConfig = dev.default_output_config().unwrap().into();
     config.buffer_size = BufferSize::Fixed(512);
+    let sample_rate = config.sample_rate.0 as f32;
+    let dt = 1.0 / sample_rate;
+    println!("{sample_rate:?}");
 
     let stream = dev
         .build_output_stream(
-            &config.into(),
+            &config,
             move |data: &mut [f32], _| {
                 for p in data.iter_mut() {
                     *p = 0.0;
                     let mut notes = notes2.lock().unwrap();
                     for n in notes.iter_mut() {
-                        n.phase += n.freq / 44100.0;
+                        n.phase += n.freq * TAU * dt;
                         if n.phase > PI {
                             n.phase -= TAU;
                         }
-                        *p += n.phase.sin() as f32 * n.vel;
+                        *p += n.phase.sin()
+                            * n.vel
+                            * if n.on {
+                                n.asdr.attack(dt)
+                            } else {
+                                n.asdr.release(dt)
+                            }
                     }
                 }
                 let mut buf = buf.lock().unwrap();
@@ -106,7 +164,7 @@ fn main() {
     eframe::run_native(
         "Riano",
         eframe::NativeOptions::default(),
-        Box::new(|_cc| Box::new(App::new(buf2))),
+        Box::new(|_cc| Box::new(App::new(buf2, notes3))),
     )
     .unwrap();
 }
@@ -114,13 +172,15 @@ fn main() {
 struct App {
     buf: Arc<Mutex<VecDeque<f32>>>,
     locked: Vec<f32>,
+    notes: Arc<Mutex<Vec<Note>>>,
 }
 
 impl App {
-    fn new(buf: Arc<Mutex<VecDeque<f32>>>) -> Self {
+    fn new(buf: Arc<Mutex<VecDeque<f32>>>, notes: Arc<Mutex<Vec<Note>>>) -> Self {
         Self {
             buf,
             locked: vec![],
+            notes,
         }
     }
 }
@@ -128,6 +188,13 @@ impl App {
 impl eframe::App for App {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
         ctx.request_repaint();
+
+        {
+            self.notes
+                .lock()
+                .unwrap()
+                .retain_mut(|n| n.asdr.release(0.0) > 0.01)
+        }
         let mut buf = self.buf.lock().unwrap();
         let to_remove = buf.len().saturating_sub(VIS_BUF_SIZE);
         let mut remainder = buf.split_off(to_remove);
