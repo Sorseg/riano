1dcab6eb472c64fba63c8e8d303ab4ea351b7643
Sorseg <sorsegg@gmail.com>
2024-06-15 21:38:02 +0300

    2d wave

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ use std::{
     f32::consts::TAU,
     sync::{
         atomic::{AtomicI32, Ordering},
+        mpsc::channel,
         Arc, Mutex,
     },
+    time::Instant,
 };
 
 use cpal::{
@@ -15,6 +17,7 @@ use cpal::{
 use derivative::Derivative;
 use eframe::egui::{CentralPanel, Slider, Ui, Vec2b};
 use egui_plot::{Line, Plot, PlotBounds, PlotPoints};
+use glam::Vec2;
 use midi_msg::{ChannelVoiceMsg, MidiMsg};
 use midir::MidiInput;
 
@@ -26,7 +29,6 @@ struct Note {
     phase: f32,
     freq: f32,
     vel: f32,
-    asdr: Asdr,
     on: bool,
     /// 0.0 - left, 1.0 - right
     pan: f32,
@@ -35,38 +37,6 @@ struct Note {
     samples_at_20_hz: Arc<[f32; 44100 / 20]>,
 }
 
-#[derive(Debug, Clone)]
-struct Asdr {
-    attack_level: f32,
-    release_level: f32,
-    attack: f32,
-    release: f32,
-}
-
-impl Asdr {
-    fn attack(&mut self, t: f32) -> f32 {
-        if self.attack_level < 1.0 {
-            self.attack_level += t / self.attack;
-        }
-
-        self.attack_level.clamp(0.0, 1.0)
-    }
-
-    fn release(&mut self, t: f32) -> f32 {
-        // TODO: separate mutation and reading
-        self.release_level *= 0.1_f32.powf(t / self.release);
-        self.release_level
-    }
-
-    fn new(attack: f32, release: f32) -> Self {
-        Self {
-            attack,
-            release,
-            attack_level: 0.0,
-            release_level: 1.0,
-        }
-    }
-}
 const VIS_BUF_SECS: f32 = 4.0;
 const VIS_BUF_SIZE: usize = (44100.0 * VIS_BUF_SECS) as usize;
 
@@ -91,11 +61,78 @@ impl Default for Settings {
     }
 }
 
+struct PianoString<const N: usize> {
+    is_active: bool,
+    tension: f32,
+    inertia: f32,
+    // x,y
+    pos: [Vec2; N],
+    vel: [Vec2; N],
+}
+impl<const N: usize> PianoString<N> {
+    fn new(tension: f32, inertia: f32) -> Self {
+        Self {
+            is_active: false,
+            tension: tension.clamp(0.1, 10.0),
+            inertia: inertia.clamp(1.0, 1000.0),
+            pos: array::from_fn(|i| Vec2::new(i as f32, 0.0)),
+            vel: array::from_fn(|_| Vec2::new(0.0, 0.0)),
+        }
+    }
+    fn pluck(&mut self, vel: f32) {
+        self.pos[20].y = vel.clamp(0.0, 10.0);
+        self.is_active = true;
+    }
+    fn tick(&mut self) {
+        if !self.is_active {
+            return;
+        }
+
+        let mut is_active = false;
+        let active_thresh = 0.0001;
+        let damping = 0.0001;
+
+        // apply velocity
+        for i in 1..(self.pos.len() - 1) {
+            self.pos[i] += self.vel[i] / self.inertia;
+            
+
+            if self.pos[i].y.abs() > active_thresh {
+                is_active = true;
+            }
+        }
+
+        // apply elasticity
+        for i in 1..(self.pos.len() - 1) {
+            let force = (self.pos[i - 1] + self.pos[i + 1]) / 2.0 - self.pos[i];
+            self.vel[i] += force * self.tension;
+            self.vel[i] *= 1.0 - damping;
+
+            if self.vel[i].y.abs() > 50.0 {
+                println!(
+                    "Runaway at tension {} inertia {}",
+                    self.tension, self.inertia
+                );
+
+                println!("{:?}", self.pos);
+                println!("{:?}", self.vel);
+
+                self.vel.iter_mut().for_each(|p| *p = Vec2::ZERO);
+                self.pos.iter_mut().for_each(|p| *p = Vec2::ZERO);
+                return;
+            }
+
+            if self.vel[i].y.abs() > active_thresh {
+                is_active = true;
+            }
+        }
+
+        self.is_active = is_active;
+    }
+}
+
 fn main() {
-    // FIXME: opt
-    let notes = Arc::new(Mutex::new(Vec::<Note>::new()));
-    let notes2 = Arc::clone(&notes);
-    let notes3 = Arc::clone(&notes);
+    let (impulse_sender, impulse_receiver) = channel();
 
     let vis_buf = Arc::new(Mutex::new(VecDeque::<f32>::new()));
     let buf2 = Arc::clone(&vis_buf);
@@ -112,10 +149,7 @@ fn main() {
         .expect("casio not connected");
 
     println!("Connecting to {}", midi_in.port_name(casio).unwrap());
-    let mut note_counter: u64 = 1;
-    let sin_pow_at_20_hz = Arc::new(array::from_fn(|i| {
-        (i as f32 * 20.0 / 44100.0 * TAU).sin().powf(3.0)
-    }));
+
     let _conn = midi_in
         .connect(
             casio,
@@ -127,64 +161,14 @@ fn main() {
                     ..
                 } = m
                 {
-                    // PRNG
-                    let detune = (note as f32 * 1.523644) % 1.0;
-                    let note = Note {
-                        note_n: note,
-                        on: true,
-                        freq: 440.0 * 2_f32.powf((note as f32 + detune * 0.1 - 69.0) / 12.0),
-                        vel: velocity as f32 / 255.0,
-                        phase: 0.0,
-                        asdr: Asdr::new(0.01, 1.0),
-                        pan: 0.5,
-                        samples_at_20_hz: Arc::clone(&sin_pow_at_20_hz),
-                    };
-
-                    let fat_n = settings_reader.fat_n.load(Ordering::Relaxed);
-
-                    for fat in 0..fat_n {
-                        // 0.0 .. 1.0
-                        let fat_id = if fat_n == 1 {
-                            0.5
-                        } else {
-                            fat as f32 / (fat_n - 1) as f32
-                        };
-
-                        let mut note = note.clone();
-                        note.freq *= 1.0
-                            + fat as f32
-                                * (settings_reader.fat_detune.load(Ordering::Relaxed) as f32
-                                    / 10000.0);
-                        note.pan = 0.5
-                            + (fat_id - 0.5)
-                                * settings_reader.fat_pan.load(Ordering::Relaxed) as f32
-                                / 10000.0;
-                        note.vel *= (1.0
-                            + settings_reader.boost.load(Ordering::Relaxed) as f32 / 10000.0)
-                            / fat_n as f32;
-                        note.phase = fat_id
-                            * (settings_reader.fat_phase_offset.load(Ordering::Relaxed) as f32
-                                / 10000.0)
-                            * TAU;
-
-                        note_counter += 1;
-                        println!("{note_counter} {note:?}");
-
-                        notes.lock().unwrap().push(note)
-                    }
+                    impulse_sender.send((note, velocity)).unwrap();
                 }
+
                 if let MidiMsg::ChannelVoice {
                     msg: ChannelVoiceMsg::NoteOff { note, .. },
                     ..
                 } = m
-                {
-                    notes.lock().unwrap().iter_mut().for_each(|n| {
-                        if n.note_n == note && n.on {
-                            n.on = false;
-                            n.asdr.release_level = n.asdr.attack_level;
-                        }
-                    })
-                }
+                {}
             },
             (),
         )
@@ -195,46 +179,49 @@ fn main() {
     let dev = host.default_output_device().unwrap();
     let mut config: cpal::StreamConfig = dev.default_output_config().unwrap().into();
     config.buffer_size = BufferSize::Fixed(256);
-    let sample_rate = config.sample_rate.0 as f32;
     println!("{config:?}");
+
+    let mut strings: [PianoString<64>; 128] =
+        array::from_fn(|i| PianoString::new(i as f32 / 10.0 - 5.0, 100.0 - i as f32 / 2.0));
+    let mut start = Instant::now();
 
     let stream = dev
         .build_output_stream(
             &config,
             move |data: &mut [f32], _| {
-                fn apply_notes_to_single_sample(
-                    d: &mut f32,
-                    pan_mul: f32,
-                    pan_offset: f32,
-                    notes: &mut [Note],
-                    sample_rate: f32,
-                ) {
-                    let dt = 1.0 / sample_rate;
-                    *d = 0.0;
-                    for n in notes.iter_mut() {
-                        n.phase += n.freq / 20.0;
-                        if n.phase as usize > n.samples_at_20_hz.len() - 1 {
-                            n.phase -= n.samples_at_20_hz.len() as f32
-                        }
-                        let idx = n.phase as usize;
-                        let sample = n.samples_at_20_hz[idx] * n.vel;
-                        let panned = sample * (pan_offset + n.pan * pan_mul);
-                        let asdrd = panned
-                            * if n.on {
-                                n.asdr.attack(dt)
-                            } else {
-                                n.asdr.release(dt)
-                            };
-
-                        *d += asdrd;
-                    }
+                while let Ok((string_n, vel)) = impulse_receiver.try_recv() {
+                    let string = &mut strings[string_n as usize];
+                    string.pluck(vel as f32 / 255.0 * 3.0);
+                    println!(
+                        "playing  string {} tension {} inertia {}",
+                        string_n,
+                        string.tension, string.inertia
+                    );
                 }
-                let mut notes = notes2.lock().unwrap();
+                let amplification = 4.0;
+
                 for c in data.chunks_exact_mut(2) {
+                    for s in &mut strings {
+                        s.tick();
+                    }
+                    if start.elapsed().as_millis() > 100 {
+                        // println!("{:?}", strings[50].pos);
+                        start = Instant::now();
+                    }
                     // left
-                    apply_notes_to_single_sample(&mut c[0], -1.0, 1.0, &mut notes, sample_rate);
+                    c[0] = strings
+                        .iter()
+                        .filter(|s| s.is_active)
+                        .map(|s| s.pos[40].y)
+                        .sum::<f32>()
+                        * amplification;
                     // right
-                    apply_notes_to_single_sample(&mut c[1], 1.0, 0.0, &mut notes, sample_rate)
+                    c[1] = strings
+                        .iter()
+                        .filter(|s| s.is_active)
+                        .map(|s| s.pos[41].y)
+                        .sum::<f32>()
+                        * amplification;
                 }
 
                 let mut vis_buf = vis_buf.lock().unwrap();
@@ -257,7 +244,6 @@ fn main() {
             Box::new(App {
                 vis_buf: buf2,
                 locked: vec![],
-                notes: notes3,
                 settings: settings_writer,
             })
         }),
@@ -268,7 +254,6 @@ fn main() {
 struct App {
     vis_buf: Arc<Mutex<VecDeque<f32>>>,
     locked: Vec<f32>,
-    notes: Arc<Mutex<Vec<Note>>>,
     settings: Arc<Settings>,
 }
 
@@ -276,19 +261,12 @@ impl eframe::App for App {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
         ctx.request_repaint();
 
-        {
-            self.notes
-                .lock()
-                .unwrap()
-                .retain_mut(|n| n.asdr.release(0.0) > 0.01)
-        }
         let mut buf = self.vis_buf.lock().unwrap();
         let to_remove = buf.len().saturating_sub(VIS_BUF_SIZE);
         for _ in 0..to_remove {
             buf.pop_front();
         }
 
-        let step_by = 20;
         CentralPanel::default().show(ctx, |ui| {
             let mut reset_zoom = false;
 
@@ -310,16 +288,16 @@ impl eframe::App for App {
                     atomic.store(val, Ordering::Relaxed);
                 }
             };
-            slider(
-                ui,
-                &self.settings.fat_phase_offset,
-                -30000..=30000,
-                "phase offset",
-            );
-            slider(ui, &self.settings.fat_detune, -10000..=10000, "detune");
-            slider(ui, &self.settings.fat_n, 1..=100, "chorun N");
-            slider(ui, &self.settings.fat_pan, -10000..=10000, "chorun pan");
-            slider(ui, &self.settings.boost, 0..=100000, "boost");
+            // slider(
+            //     ui,
+            //     &self.settings.fat_phase_offset,
+            //     -30000..=30000,
+            //     "phase offset",
+            // );
+            // slider(ui, &self.settings.fat_detune, -10000..=10000, "detune");
+            // slider(ui, &self.settings.fat_n, 1..=100, "chorun N");
+            // slider(ui, &self.settings.fat_pan, -10000..=10000, "chorun pan");
+            // slider(ui, &self.settings.boost, 0..=100000, "boost");
 
             Plot::new("waveform")
                 .auto_bounds(Vec2b { x: true, y: false })
@@ -331,10 +309,19 @@ impl eframe::App for App {
                             [VIS_BUF_SECS as f64, 1.0],
                         ))
                     }
-                    let live: PlotPoints = buf
+                    let step_by = 20;
+                    let left: PlotPoints = buf
                         .iter()
                         .enumerate()
                         .map(|(i, s)| [i as f64 / 44100.0, *s as f64])
+                        .step_by(step_by)
+                        .collect();
+
+                    let right: PlotPoints = buf
+                        .iter()
+                        .enumerate()
+                        .map(|(i, s)| [i as f64 / 44100.0, *s as f64])
+                        .skip(1)
                         .step_by(step_by)
                         .collect();
 
@@ -342,10 +329,12 @@ impl eframe::App for App {
                         .locked
                         .iter()
                         .enumerate()
+                        .step_by(2)
                         .map(|(i, s)| [i as f64 / 44100.0, *s as f64])
                         .collect();
 
-                    ui.line(Line::new(live));
+                    ui.line(Line::new(left).name("left"));
+                    ui.line(Line::new(right).name("right"));
                     ui.line(Line::new(locked).name("locked"));
                 });
         });
